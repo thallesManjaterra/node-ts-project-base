f48bda64a54db8c355a13c392df9c0550ef666e6
Thalles Manjaterra <thalles.devraiz@gmail.com>
2022-04-26 18:46:24 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
